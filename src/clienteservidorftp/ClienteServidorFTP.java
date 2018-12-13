3a74b1759ec68ede83410b4a1541832d645307c5
Guillermo Ramos Alberti <agamenon1224@gmail.com>
2018-12-13 09:27:00 +0100

    Second Commit
    Init project

---
+++ src/clienteservidorftp/ClienteServidorFTP.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clienteservidorftp;
+
+/**
+ *
+ * @author Guillermo Ramos Alberti <i>"Ervo"</i>
+ */
+public class ClienteServidorFTP {
+
+	/**
+	 * @param args the command line arguments
+	 */
+	public static void main(String[] args) {
+		// TODO code application logic here
+	}
+
+}
