4b6118264b6e8957fd2783f7df456daab67b2646
Guillermo Ramos Alberti <agamenon1224@gmail.com>
2018-12-19 19:51:33 +0100

    Login changed

--- DiseñoGuille/jsCorreo.js
+++ DiseñoGuille/jsCorreo.js
@@ -1,67 +1,52 @@
 window.addEventListener('load', inicio, false);
 
 function inicio() {
-    genera_tabla();
+	genera_tabla();
 }
+
 function genera_tabla() {
-    // Obtener la referencia del elemento body
-    var body = document.getElementsByTagName("body")[0];
-    var div = document.getElementById("principal");
-
-    // Crea un elemento <table>, un elemento <thead> y un elemento <tbody>
-    var tabla = document.createElement("table");
-    var thTitulo = document.createElement("thead");
-    var tblBody = document.createElement("tbody");
-
-    //Crear la hilera del titulo
-    var cabecera = document.createElement("tr");
-    //Crear elemenro <th> y un nodo texto
-    var titulo = document.createElement("th");
-    var nomTitulo = document.createTextNode("Correos");
-    titulo.appendChild(nomTitulo);
-
-    //Añadir la cabecera a la tabla
-    thTitulo.appendChild(titulo);
-    //Añadir la cabecera a la tabla
-    tabla.appendChild(thTitulo);
-
-    // Crea las celdas (Tantas celdas como archivos contenga el servidor) 
-    for (var i = 0; i < 4; i++) {
-        // Crea las hileras de la tabla
-        var hilera = document.createElement("tr");
-        hilera.onclick = function () {
-            var opcion = confirm("¿ Descargar Archivo?");
-            if (opcion == true) {
-                console.log("You pressed OK!");
-                var divBarra = document.getElementById("tercero");
-                divBarra.innerHTML = document.getElementById("barraDescarga").innerHTML;
-                body.appendChild(divBarra);
-            } else {
-                console.log("You pressed Cancel!");
-                var divBarra = document.getElementById("tercero");
-                divBarra.innerHTML = " ";
-                body.appendChild(divBarra);
-            }
-        };
-        //Crear elemento <td>
-        var celda = document.createElement("td");
-        //Crear contenido de la tabla
-        var textoCelda = document.createTextNode("Aquí va los nombres de los archivos.");
-        //Insertar el contenido en el elemento <td>
-        celda.appendChild(textoCelda);
-        //Insertar el elemento <td> en el elemento <tr>
-        hilera.appendChild(celda);
-        // Agrega la fila a la tabla
-        tblBody.appendChild(hilera);
-    }
-
-    // posiciona el <tbody> debajo del elemento <table>
-    tabla.appendChild(tblBody);
-    //Añadir la <table> al <div> correspondiente
-    div.appendChild(tabla);
-    // appends <div> into <body>
-    body.appendChild(div);
-    // modifica el atributo "border" de la tabla y lo fija a "2";
-    tabla.setAttribute("id", "tabla");
-    document.getElementById("tabla").className = "table table-hover";
+	// Obtener la referencia del elemento body
+	var body = document.getElementsByTagName("body")[0];
+	var div = document.getElementById("principal");
+	// Crea un elemento <table>, un elemento <thead> y un elemento <tbody>
+	var tabla = document.createElement("table");
+	var thTitulo = document.createElement("thead");
+	var tblBody = document.createElement("tbody");
+	//Crear la hilera del titulo
+	var cabecera = document.createElement("tr");
+	//Crear elemenro <th> y un nodo texto
+	var titulo = document.createElement("th");
+	var nomTitulo = document.createTextNode("Correos");
+	titulo.appendChild(nomTitulo);
+	//Añadir la cabecera a la tabla
+	thTitulo.appendChild(titulo);
+	//Añadir la cabecera a la tabla
+	tabla.appendChild(thTitulo);
+	// Crea las celdas (Tantas celdas como archivos contenga el servidor) 
+	for (var i = 0; i < 4; i++) {
+		// Crea las hileras de la tabla
+		var hilera = document.createElement("tr");
+		hilera.onclick = function() {
+			// TODO: Rellenar aquí
+		};
+		//Crear elemento <td>
+		var celda = document.createElement("td");
+		//Crear contenido de la tabla
+		var textoCelda = document.createTextNode("Aquí va los nombres de los archivos.");
+		//Insertar el contenido en el elemento <td>
+		celda.appendChild(textoCelda);
+		//Insertar el elemento <td> en el elemento <tr>
+		hilera.appendChild(celda);
+		// Agrega la fila a la tabla
+		tblBody.appendChild(hilera);
+	}
+	// posiciona el <tbody> debajo del elemento <table>
+	tabla.appendChild(tblBody);
+	//Añadir la <table> al <div> correspondiente
+	div.appendChild(tabla);
+	// appends <div> into <body>
+	body.appendChild(div);
+	// modifica el atributo "border" de la tabla y lo fija a "2";
+	tabla.setAttribute("id", "tabla");
+	document.getElementById("tabla").className = "table table-hover";
 }
