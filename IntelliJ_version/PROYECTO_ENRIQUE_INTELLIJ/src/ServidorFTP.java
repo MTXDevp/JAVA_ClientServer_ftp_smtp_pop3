ec62dcf5462bbea932c4e93f322f717911b456bd
Rafael Valls Sanchez <rafa-valls@hotmail.com>
2018-12-20 11:11:37 +0100

    Version Intellij Update MTX

--- IntelliJ_version/PROYECTO_ENRIQUE_INTELLIJ/src/ServidorFTP.java
+++ IntelliJ_version/PROYECTO_ENRIQUE_INTELLIJ/src/ServidorFTP.java
@@ -1,4 +1,12 @@
+
+
 public class ServidorFTP {
+
+    //FTPClient client = new FTPClient();
+
+    String sFTP = "ftp.miservidor.com";
+    String sUser = "usuario";
+    String sPassword = "password";
 
     public ServidorFTP(){
 
