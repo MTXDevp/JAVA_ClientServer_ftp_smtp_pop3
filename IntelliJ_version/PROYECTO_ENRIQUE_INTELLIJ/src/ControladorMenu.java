5328282385af4de312d60a112af8f78d5bea19ea
Rafael Valls Sanchez <rafa-valls@hotmail.com>
2018-12-20 10:07:02 +0100

    Version Intellij Update MTX

---
+++ IntelliJ_version/PROYECTO_ENRIQUE_INTELLIJ/src/ControladorMenu.java
@@ -0,0 +1,23 @@
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
+import com.teamdev.jxbrowser.chromium.Browser;
+import com.teamdev.jxbrowser.chromium.JSValue;
+import com.teamdev.jxbrowser.chromium.events.FinishLoadingEvent;
+import com.teamdev.jxbrowser.chromium.events.LoadAdapter;
+import com.teamdev.jxbrowser.chromium.events.LoadListener;
+
+public class ControladorMenu {
+
+    public ControladorMenu(Browser b){
+
+        System.out.println("Estoy en el controlador Menu");
+
+
+
+
+        /*
+        Browser browser = event.getBrowser();
+        JSValue value = browser.executeJavaScriptAndReturnValue("window");
+        value.asObject().setProperty("Account", new ControladorLogin.getDatosRegistro());
+*/
+    }
+}
---
+++ IntelliJ_version/PROYECTO_ENRIQUE_INTELLIJ/src/html/ConexionFireBase.js
@@ -0,0 +1,8 @@
+window.onload = function(){
+
+document.getElementById("botonRegistrar").addEventListener("click", function(){
+    alert("LA PUTA");
+});
+};
+
+
