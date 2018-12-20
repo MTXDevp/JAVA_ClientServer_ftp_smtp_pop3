2e7ad45164e2413a5770a8982bae946de2024251
Rafael Valls Sanchez <rafa-valls@hotmail.com>
2018-12-20 12:11:32 +0100

    Version Intellij Update MTX

--- IntelliJ_version/PROYECTO_ENRIQUE_INTELLIJ/src/Main.java
+++ IntelliJ_version/PROYECTO_ENRIQUE_INTELLIJ/src/Main.java
@@ -18,7 +18,8 @@ public class Main {
     public static void main(String[] args) {
 
 
-        ControladorLogin cl = new ControladorLogin();
+        ServidorFTP ftp = new ServidorFTP();
+        //ControladorLogin cl = new ControladorLogin();
 
     }
 
