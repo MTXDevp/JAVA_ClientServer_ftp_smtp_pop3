0b9df206d053222b797dac406801e966236c69df
Guillermo Barcia Molina <gbarciamolina@gmail.com>
2019-01-13 21:53:59 +0100

    Diseño subir archivos (Progress Bar)

---
+++ VERSION FINAL ECLIPSE/ServidorFtpStmpPop3Maven/src/main/java/Peval4/ServidorFtpStmpPop3Maven/Disenio/Js/SubirArchivo.js
@@ -0,0 +1,23 @@
+window.addEventListener('load', inicio, false);
+
+function inicio() { }
+
+function Subir() {
+    var body = document.getElementsByTagName("body")[0];
+    var opcion = confirm("¿Subir Archivo?");
+    if (opcion == true) {
+        SentDatosFTP();
+        var divBarra = document.getElementById("segundario");
+        divBarra.innerHTML = document.getElementById("barraDescarga").innerHTML;
+        body.appendChild(divBarra);
+    } else {
+        var divBarra = document.getElementById("segundario");
+        divBarra.innerHTML = " ";
+        body.appendChild(divBarra);
+    }
+}
+
+function SentDatosFTP() {
+    var direccionFTP = formFTP.elements['direccionFTP'].value;
+    Account.save(direccionFTP);
+}
