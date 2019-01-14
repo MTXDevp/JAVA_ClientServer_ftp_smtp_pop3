c32b97357e2a1f0507958a6225e7f601953b8b26
Guillermo Barcia Molina <gbarciamolina@gmail.com>
2019-01-14 13:26:15 +0100

    Barra carga correo(Arreglar)

---
+++ VERSION FINAL ECLIPSE/ServidorFtpStmpPop3Maven/src/main/java/Peval4/ServidorFtpStmpPop3Maven/Disenio/Js/menu.js
@@ -0,0 +1,16 @@
+window.addEventListener('load', inicio, false);
+
+function inicio() {
+}
+
+function myLinkedin(numero) {
+	if (numero == 1) {
+		window.open("https://es.linkedin.com");
+	}
+	else if (numero == 2) {
+		window.open("https://www.google.com");
+	}
+	else if (numero == 3) {
+		window.open("https://outlook.live.com");
+	}
+}
