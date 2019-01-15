4ebdbf033e7fb4f7af7990954569fd90279312dd
Guillermo Ramos Alberti <agamenon1224@gmail.com>
2019-01-15 22:49:06 +0100

    Code formated

--- VERSION FINAL ECLIPSE/ServidorFtpStmpPop3Maven/src/main/java/Peval4/ServidorFtpStmpPop3Maven/Disenio/Js/VisualizarCorreoConcreto.js
+++ VERSION FINAL ECLIPSE/ServidorFtpStmpPop3Maven/src/main/java/Peval4/ServidorFtpStmpPop3Maven/Disenio/Js/VisualizarCorreoConcreto.js
@@ -4,15 +4,11 @@ function inicio() {
 	GetDatos();
 }
 
-function GetDatos(){
-
+function GetDatos() {
 	var remitente = localStorage.getItem("remitenteSeleccionado");
 	var asunto = localStorage.getItem("asuntoSeleccionado");
 	var cuerpo = localStorage.getItem("cuerpoSeleccionado");
-
 	document.getElementById("remitente").value = remitente;
 	document.getElementById("asunto").value = asunto;
 	document.getElementById("contenidoCorreo").value = cuerpo;
-
-
 }
--- VERSION FINAL ECLIPSE/ServidorFtpStmpPop3Maven/src/main/java/Peval4/ServidorFtpStmpPop3Maven/Disenio/Js/verCorreo.js
+++ VERSION FINAL ECLIPSE/ServidorFtpStmpPop3Maven/src/main/java/Peval4/ServidorFtpStmpPop3Maven/Disenio/Js/verCorreo.js
@@ -1,17 +1,10 @@
 window.addEventListener('load', inicio, false);
 
 function inicio() {
-
-        alert('estoy en ver Correo');
-
-       var cuerpo = localStorage.cuerpoSeleccionado;
-       var findCorchetes = "\\[";
-       var re = new RegExp(findCorchetes, 'g');
-       var cuerpoClear = cuerpo.replace(re, '');
-       document.getElementById("contenidoCorreo").value = cuerpoClear;
-
-
-
-
-
+	alert('estoy en ver Correo');
+	var cuerpo = localStorage.cuerpoSeleccionado;
+	var findCorchetes = "\\[";
+	var re = new RegExp(findCorchetes, 'g');
+	var cuerpoClear = cuerpo.replace(re, '');
+	document.getElementById("contenidoCorreo").value = cuerpoClear;
 }
